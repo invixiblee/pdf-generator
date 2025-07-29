1f4a091a068dd7ef49b6650fc078f17d49ae105b
Tazir <tazirxxx@gmail.com>
2025-07-29 15:49:42 +0300

    Create batch PDFs (with header, line, and footer)

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='P',unit='mm', format='A4', )
+pdf.set_auto_page_break(auto=False, margin=0)
+df = pd.read_csv('topics.csv')
 
-pdf.add_page()
+for index, row in df.iterrows():
+    # Master Page
+    pdf.add_page()
 
-pdf.set_font(family='Times', style='B', size=12)
-pdf.cell(w=0, h=12, txt='hello world', align='L', ln=1)
-pdf.set_font(family='Times', style='B', size=10)
-pdf.cell(w=0, h=12, txt='hello again', align='L', ln=1)
+    # Header
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+
+    # Line
+    pdf.line(10, 21, 200, 21)
+
+    # Footer
+    pdf.ln(265)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+    # Secondry Pages
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+
+        # Footer
+        pdf.ln(277)
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
 
 pdf.output('output.pdf')
